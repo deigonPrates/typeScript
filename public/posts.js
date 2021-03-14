a8281ef48b1bc0e6127b56ee34dde28394d74317
Deigon Prates <deigon_prates@hotmail.com>
2021-03-14 20:10:32 -0300

    Crie variáveis

--- public/posts.js
+++ public/posts.js
@@ -1,47 +1,51 @@
 var BoxPostList = /** @class */ (function () {
     function BoxPostList() {
+        this.buttonListSelector = "#" + BoxPostList.boxId + ">button[type=button]";
         this.init();
     }
     BoxPostList.prototype.init = function () {
         var _this = this;
-        var buttonList = document.querySelector('#box-post-list>button[type=button]');
+        var buttonList = document.querySelector(this.buttonListSelector);
         buttonList.addEventListener('click', function () {
             _this.hiddenBox();
-            var boxForm = document.getElementById('box-post-form');
+            var boxForm = document.getElementById(BoxPostForm.boxId);
             boxForm.removeAttribute('style');
         });
     };
     BoxPostList.prototype.hiddenBox = function () {
-        var boxList = document.getElementById('box-post-list');
+        var boxList = document.getElementById(BoxPostList.boxId);
         boxList.style.display = 'none';
     };
     BoxPostList.prototype.showBox = function () {
-        var boxList = document.getElementById('box-post-list');
+        var boxList = document.getElementById(BoxPostList.boxId);
         boxList.removeAttribute('style');
     };
+    BoxPostList.boxId = 'box-post-list';
     return BoxPostList;
 }());
 var BoxPostForm = /** @class */ (function () {
     function BoxPostForm() {
+        this.buttonFormSelector = "#" + BoxPostForm.boxId + ">button[type=button]";
         this.init();
     }
     BoxPostForm.prototype.init = function () {
         var _this = this;
-        var buttonForm = document.querySelector('#box-post-form>button[type=button]');
+        var buttonForm = document.querySelector(this.buttonFormSelector);
         buttonForm.addEventListener('click', function () {
             _this.hiddenBox();
-            var boxList = document.getElementById('box-post-list');
+            var boxList = document.getElementById(BoxPostList.boxId);
             boxList.removeAttribute('style');
         });
     };
     BoxPostForm.prototype.hiddenBox = function () {
-        var boxForm = document.getElementById('box-post-form');
+        var boxForm = document.getElementById(BoxPostForm.boxId);
         boxForm.style.display = 'none';
     };
     BoxPostForm.prototype.showBox = function () {
-        var boxForm = document.getElementById('box-post-form');
+        var boxForm = document.getElementById(BoxPostForm.boxId);
         boxForm.removeAttribute('style');
     };
+    BoxPostForm.boxId = 'box-post-form';
     return BoxPostForm;
 }());
 new BoxPostForm();
--- src/posts.ts
+++ src/posts.ts
@@ -1,47 +1,53 @@
 class BoxPostList{
+    static boxId = 'box-post-list';
+    private buttonListSelector = `#${BoxPostList.boxId}>button[type=button]`;
+
     constructor(){
         this.init();
     }
 
     private init(){
-        const buttonList = document.querySelector('#box-post-list>button[type=button]');
+        const buttonList = document.querySelector(this.buttonListSelector);
         buttonList.addEventListener('click', () => {
             this.hiddenBox();  
 
-            const boxForm = document.getElementById('box-post-form');
+            const boxForm = document.getElementById(BoxPostForm.boxId);
             boxForm.removeAttribute('style');
         });
     }
     hiddenBox(){
-        const boxList = document.getElementById('box-post-list');
+        const boxList = document.getElementById(BoxPostList.boxId);
         boxList.style.display = 'none';
     }
     showBox(){
-        const boxList = document.getElementById('box-post-list');
+        const boxList = document.getElementById(BoxPostList.boxId);
         boxList.removeAttribute('style');
     }
 }
 
 class BoxPostForm{
+    static boxId = 'box-post-form';
+    private buttonFormSelector = `#${BoxPostForm.boxId}>button[type=button]`;
+
     constructor(){
         this.init();
     }
 
     private init(){
-        const buttonForm = document.querySelector('#box-post-form>button[type=button]');
+        const buttonForm = document.querySelector(this.buttonFormSelector);
         buttonForm.addEventListener('click', () => {
             this.hiddenBox();  
 
-            const boxList = document.getElementById('box-post-list');
+            const boxList = document.getElementById(BoxPostList.boxId);
             boxList.removeAttribute('style');
         });
     }
     hiddenBox(){
-        const boxForm = document.getElementById('box-post-form');
+        const boxForm = document.getElementById(BoxPostForm.boxId);
         boxForm.style.display = 'none';
     }
     showBox(){
-        const boxForm = document.getElementById('box-post-form');
+        const boxForm = document.getElementById(BoxPostForm.boxId);
         boxForm.removeAttribute('style');
     }
 }
