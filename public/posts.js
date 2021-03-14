f5a380490ce88a78e6a02430795c95414e8baea3
Deigon Prates <deigon_prates@hotmail.com>
2021-03-14 18:17:32 -0300

    Assim não precisa usar o TypeScript

--- public/posts.js
+++ public/posts.js
@@ -1,1 +1,14 @@
-var desc = "descricao";
+var buttonForm = document.querySelector('#box-post-form>button[type=button]');
+buttonForm.addEventListener('click', function () {
+    var boxForm = document.getElementById('box-post-form');
+    boxForm.style.display = 'none';
+    var boxList = document.getElementById('box-post-list');
+    boxList.removeAttribute('style');
+});
+var buttonList = document.querySelector('#box-post-list>button[type=button]');
+buttonList.addEventListener('click', function () {
+    var boxList = document.getElementById('box-post-list');
+    boxList.style.display = 'none';
+    var boxForm = document.getElementById('box-post-form');
+    boxForm.removeAttribute('style');
+});
--- src/posts.ts
+++ src/posts.ts
@@ -1,1 +1,17 @@
-let desc = "descricao";
+const buttonForm = document.querySelector('#box-post-form>button[type=button]');
+buttonForm.addEventListener('click', () =>{
+    const boxForm = document.getElementById('box-post-form');
+    boxForm.style.display = 'none';
+
+    const boxList = document.getElementById('box-post-list');
+    boxList.removeAttribute('style');
+});
+
+const buttonList = document.querySelector('#box-post-list>button[type=button]');
+buttonList.addEventListener('click', () =>{
+    const boxList = document.getElementById('box-post-list');
+    boxList.style.display = 'none';
+
+    const boxForm = document.getElementById('box-post-form');
+    boxForm.removeAttribute('style');
+});
